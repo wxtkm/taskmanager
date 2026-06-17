9c5ad27b4424387e17932d3cc70662da1f618607
taskmanager <dimakovtun277@gmail.com>
2026-06-17 13:08:01 +0200

    enable web security for SecurityConfig

--- src/main/java/com/wxtkm/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/wxtkm/taskmanager/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import com.wxtkm.taskmanager.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
